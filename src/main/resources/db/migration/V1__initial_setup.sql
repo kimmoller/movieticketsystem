c7753eb6fbb9cfcbb942c3394eb3e883a01a7eb0
Kim Möller <kim.moller2@protonmail.com>
2024-05-29 14:05:45 +0200

    Add missing unique tag

--- src/main/resources/db/migration/V1__initial_setup.sql
+++ src/main/resources/db/migration/V1__initial_setup.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS identity(
     id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     date_of_birth TIMESTAMPTZ NOT NULL,
